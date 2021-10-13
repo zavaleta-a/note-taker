ebfa49be7fe3e5ee4df8bd765f9e1621770395e8
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-13 03:46:36 -0500

    created sample text in db.json as existing notes

--- express.js
+++ express.js
@@ -5,7 +5,7 @@
 // Setup
 const express = require("express");
 const path = require("path");
-const data = require("./Develop/db/db.json");
+const data = require("./Develop/db/db");
 // Must be able to write and save notes (use fs module to write and receive notes)
 const fs = require("fs");
 
@@ -43,13 +43,27 @@ app.get("*", (req, res) => {
 // Create API routes;
 
 //  GET /api/notes (should read db.json and return all saved notes)
-app.get("/api/notes", (req, res) => res.json(notes));
+app.get("/api/notes", (req, res) => {
+  res.json(data);
+});
 // POST /api/notes (should receive new note, add it to the db.json, then return the new note)
 app.post("/api/notes", (req, res) => {
   let newNote = req.body;
   notes.push(newNote);
+  updateDb();
   console.log("new note added!");
 });
 
+app.get("/api/notes/:id", (req, res) => {
+  res.json(ntoes[req.params.id]);
+});
+
+// update json with new notes
+function updateDb() {
+  fs.writeFile("db/db.json", JSON.stringify(notes, "/t"), (err) => {
+    if (err) throw err;
+    return true;
+  });
+}
 // Set up listener to PORT
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
