a68a16affe3284cf0d0e05391d16b45d2dc08865
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-13 18:47:37 -0500

    resolved errors shown in terminal

--- express.js
+++ express.js
@@ -5,14 +5,14 @@
 // Setup
 const express = require("express");
 const path = require("path");
-const data = require("./Develop/db/db");
+const data = require("./develop/db/db.json");
 // Must be able to write and save notes (use fs module to write and receive notes)
 const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, "Develop", "public")));
+app.use(express.static(path.join(__dirname, "develop/public")));
 
 // Data parsing (middleware)
 app.use(express.urlencoded({ extended: true }));
@@ -21,21 +21,19 @@ app.use(express.json());
 //---------------------------------------
 
 // Create routes;
-app.get("./public/.notes.html", (req, res) => {
-  res.send("Navigate to /notes.html");
-});
+
 // GET /notes should return notes.html
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "develop/public/notes.html"));
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "develop/public/index.html"));
 });
 
 // GET * should return index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join__dirname, "/Develop/public/index.html");
+  res.sendFile(path.join(__dirname, "develop/public/index.html"));
 });
 
 //---------------------------------
@@ -44,7 +42,8 @@ app.get("*", (req, res) => {
 
 //  GET /api/notes (should read db.json and return all saved notes)
 app.get("/api/notes", (req, res) => {
-  res.json(data);
+  let notes = JSON.parse(fs.readFileSync("./develop/db/db.json"));
+  res.json(notes);
 });
 // POST /api/notes (should receive new note, add it to the db.json, then return the new note)
 app.post("/api/notes", (req, res) => {
@@ -54,13 +53,9 @@ app.post("/api/notes", (req, res) => {
   console.log("new note added!");
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  res.json(ntoes[req.params.id]);
-});
-
 // update json with new notes
 function updateDb() {
-  fs.writeFile("db/db.json", JSON.stringify(notes, "/t"), (err) => {
+  fs.writeFile("./develop/db/db.json", JSON.stringify(notes), (err) => {
     if (err) throw err;
     return true;
   });
