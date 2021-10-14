c49a6f4a12416d277fd53744ce96fdc40ad37f3f
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-13 22:25:18 -0500

    fixed issue where api was not being triggered

--- express.js
+++ express.js
@@ -5,10 +5,12 @@
 // Setup
 const express = require("express");
 const path = require("path");
-const data = require("./develop/db/db.json");
+const data = require("./develop/db/db");
+
 // Must be able to write and save notes (use fs module to write and receive notes)
 const fs = require("fs");
-
+// linking routes
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -17,48 +19,8 @@ app.use(express.static(path.join(__dirname, "develop/public")));
 // Data parsing (middleware)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(routes);
 //---------------------------------------
 
-// Create routes;
-
-// GET /notes should return notes.html
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "develop/public/notes.html"));
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "develop/public/index.html"));
-});
-
-// GET * should return index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "develop/public/index.html"));
-});
-
-//---------------------------------
-
-// Create API routes;
-
-//  GET /api/notes (should read db.json and return all saved notes)
-app.get("/api/notes", (req, res) => {
-  let notes = JSON.parse(fs.readFileSync("./develop/db/db.json"));
-  res.json(notes);
-});
-// POST /api/notes (should receive new note, add it to the db.json, then return the new note)
-app.post("/api/notes", (req, res) => {
-  let newNote = req.body;
-  notes.push(newNote);
-  updateDb();
-  console.log("new note added!");
-});
-
-// update json with new notes
-function updateDb() {
-  fs.writeFile("./develop/db/db.json", JSON.stringify(notes), (err) => {
-    if (err) throw err;
-    return true;
-  });
-}
 // Set up listener to PORT
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
---
+++ routes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const router = express.Router();
+
+const path = require("path");
+
+const fs = require("fs");
+// Create routes;
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+// GET /notes should return notes.html
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+});
+
+// // GET * should return index.html
+
+//---------------------------------
+
+// Create API routes;
+
+//  GET /api/notes (should read db.json and return all saved notes)
+router.get("/api/notes", (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./develop/db/db.json"));
+  res.json(notes);
+  console.log("si");
+});
+// POST /api/notes (should receive new note, add it to the db.json, then return the new note)
+router.post("/api/notes", (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./develop/db/db.json"));
+  let newNote = req.body;
+  notes.push(newNote);
+  updateDb(notes);
+  console.log("new note added!");
+});
+
+// update json with new notes
+function updateDb(notes) {
+  fs.writeFile("./develop/db/db.json", JSON.stringify(notes), (err) => {
+    if (err) throw err;
+    return true;
+  });
+}
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+module.exports = router;
